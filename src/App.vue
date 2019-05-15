c7aae82716c50266f4fee43b266b5a0c49fecbea
Aleksander Dudek <aleksander.dudek@tieto.com>
2019-05-15 16:15:35 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <main>
+          <HomePage />
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HomePage from './components/HomePage.vue';
+
+@Component({
+  components: {
+    HomePage,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+</style>
