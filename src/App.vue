5f301bdb5dbddba5725892d46ada3c30994007c2
Aleksander Dudek <aleksander.dudek@tieto.com>
2019-05-16 16:12:14 +0200

    Works

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png" />
+            Build a bot
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
-          <HomePage />
+          <RobotBuilder />
     </main>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HomePage from './components/HomePage.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './builder/RobotBuilder.vue';
 
 @Component({
   components: {
-    HomePage,
+    // HomePage,
+    RobotBuilder,
   },
 })
 export default class App extends Vue {}
@@ -34,5 +46,25 @@ main {
   background-color: white;
   width: 1024px;
   min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
